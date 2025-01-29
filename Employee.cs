08ceec714ec2b8680eddea50425f0faa4c29c31b
Omar <Omar@DESKTOP-OUKQKJG>
2025-01-29 07:23:39 +0300

    video 1

---
+++ Employee.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace adv2
+{
+       class Employee : IComparable
+    {
+        public int Compare(object? x , object? y)
+        {
+            Employee X = (Employee)x;
+            Employee Y = (Employee)y;
+            return X.Name.CompareTo(Y.Name);
+        }
+    }
+    internal class Employee3 : IComparable
+    {
+        public int Id { get; set; }
+        public  string Name { get; set; }
+        public int Age { get; set; }
+        public  double Salary { get; set; }
+
+        public int CompareTo(object? other)
+        {
+            Employee E = (Employee) other;
+            return this.Age.CompareTo(E.Age);
+        }  
+        public override string ToString()
+        {
+            return $"{Id} {Name} , {Age} {Salary}";
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,10 +1,76 @@
-﻿namespace adv2
+﻿using System.Collections;
+
+namespace adv2
 {
     internal class Program
     {
+        public static void printarraylist(ArrayList arraylist) {
+           
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region  video 1 
+            //    ArrayList list = new ArrayList();
+            //    list.Add(1);
+            //    //Console.WriteLine( $"{list .Count} , {list.Capacity} ");
+            //    list.Add(2);
+            //    list.Add(3);
+            //    list.Add(4);
+            //Console.WriteLine($"{list.Count} , {list.Capacity} ");
+            //list.Add(5);
+            //Console.WriteLine($"{list.Count} , {list.Capacity} ");
+            //list.Add(4);
+            //list.Add(4);
+            //list.Add(4);
+            //list.Add(4);
+            //Console.WriteLine($"{list.Count} , {list.Capacity} ");
+            //list.Add(4);
+
+            //Console.WriteLine($"{list.Count} , {list.Capacity} ");
+            //Console.WriteLine(list.IsFixedSize);
+            //Console.WriteLine(list.IsReadOnly);
+            //Console.WriteLine(list[2]);
+
+            //list.AddRange(new[] { 10, 11, 12 });
+            //printarraylist(list);
+            //int index = list.BinarySearch(2, );
+            //Console.WriteLine(index);
+
+            //Employee3 e01 = new Employee3() { Id = 1, Name = "ahmed", Salary = 1200, Age = 10 };
+            //Employee3 e02 = new Employee3() { Id = 2, Name = "omar", Salary = 1300, Age = 20 };
+            //Employee3 e03 = new Employee3() { Id = 3, Name = "mohamed", Salary = 1400, Age = 30 };
+            //Employee3 e04 = new Employee3() { Id = 4, Name = "amr", Salary = 1500, Age = 40 };
+            //ArrayList list2 = new ArrayList();
+            ////list2.AddRange(new Employee[] { e01, e02, e03, e04 });
+
+            //foreach(Employee item in list2)
+            //{
+            //    //Console.WriteLine(item);
+            //}
+
+            //int index = list.BinarySearch(e03);
+            //Console.WriteLine($" index = {index}");
+
+            ArrayList list3 = new ArrayList();
+            list3.Add(4);
+            list3.Add(4);
+            list3.Add(4);
+            list3.Add(4);
+            list3.Clear();
+
+            bool flag = list3.Contains(20);
+            Console.WriteLine(list3);
+
+            int[] arr = new int[4];
+            list3.CopyTo(arr, 3);
+
+            foreach (int item in arr) {
+                Console.WriteLine(item);
+            }
+            int i = list3.IndexOf(3);
+            Console.WriteLine(i);
+            #endregion
         }
-    }
+    } 
+}
 }
